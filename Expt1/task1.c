f1f8a003e213978ccdd37d9e6406322843ffe41a
Pratikjaiswar <pratikjaiswar@eng.rizvi.edu.in>
2024-10-16 10:38:03 +0530

    Create task1.c

---
+++ Expt1/task1.c
@@ -0,0 +1,30 @@
+/* WAP to print your name, age, class, division and UIN on the screen. Use
+ escape sequences to properly format the output.
+ NAME: PRATIK JAISWAR
+ Roll.no:59
+ div:E
+ class: ECS
+ UIN: 241S077
+ */
+ #include<stdio.h>
+ 
+ int main()
+ {
+ printf("Name\t: PRATIK JAISWAR\n");
+ printf("roll.no\t: 59\n");
+ printf("div\t: E\n");
+ printf("age\t: 17 \n");
+ printf("UIN\t: 241S077\n");
+ printf("class\t: ECS\n");
+ 
+ return 0;
+ }
+ 
+ /* output:-
+ Name : PRATIK JAISWAR
+ roll.no : 59
+ div : E
+ age : 17
+ UIN : 241S077
+ class : ECS
+*/
