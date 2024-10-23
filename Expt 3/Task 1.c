2df2e36d83eb7f463e169c1b00b465a5770ae9c3
Pratikjaiswar <pratikjaiswar@eng.rizvi.edu.in>
2024-10-23 10:05:59 +0530

    Create Task 1.c

---
+++ Expt 3/Task 1.c
@@ -0,0 +1,58 @@
+/*
+   NAME : PRATIK JAISWAR
+   ROLL NO : 59
+   UIN : 241S077
+   BRANCH : ECS
+*/
+
+#include<stdio.h>
+int main()
+{
+  int num ;
+  printf("Enter value");
+  scanf("%d", &num);
+  switch (num) {
+  case 1:
+    printf("value is 1");
+    break;
+  case 2 :
+    printf("value is 2");
+    break;
+  case 3 :
+    printf("value is 3");
+    break;
+  case 4:
+    printf("value is 4");
+    break;
+    case 5:
+    printf("value is 5");
+    break;
+    case 6:
+    printf("value is 6");
+    break;
+    case 7:
+    printf("value is 7");
+    break;
+    case 8:
+    printf("value is 8");
+    break;
+    case 9:
+    printf("value is 9");
+    break;
+    case 10:
+    printf("value is 10");
+    break;
+  default:
+    printf("out of range");
+    break;
+
+}
+return 0;
+}
+
+/* OUTPUT
+ Enter value 8
+ value is 8
+ Enter value 9
+ value is 9
+*/
