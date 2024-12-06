de74f8b663881d2d0b9677c5032a3e70668b78d8
Pratikjaiswar <pratikjaiswar@eng.rizvi.edu.in>
2024-12-06 22:45:09 +0530

    Update task 2.c

--- Expt 10/task 2.c
+++ Expt 10/task 2.c
@@ -1,7 +1,33 @@
 /*
-EXP NO : 9.1
+EXP NO : 10.2
  TITLE : WAP to print the elements of an array in reverse order using pointers 
  UIN :241S077
  Roll.No:59
  CLASS & DIV :F.E -ECS ENG
+ */
+#include <stdio.h>
+int main() {
+ int n;
+ printf("Enter the number of elements in the array: ");
+ scanf("%d", &n);
+ int arr[n];
+ int *ptr = arr;
+ printf("Enter %d elements:\n", n);
+ for (int i = 0; i < n; i++) {
+ scanf("%d", ptr + i);
+ }
+ printf("Array elements in reverse order:\n");
+ for (int i = n - 1; i >= 0; i--) {
+ printf("%d ", *(ptr + i));
+ }
+ printf("\n");
+ return 0;
+}
+ 
+ /* OUTPUT:-
+ Enter the number of elements in the array: 5
+ Enter 5 elements:
+ 1 2 3 4 5
+ Array elements in reverse order:
+ 5 4 3 2 1
  */
