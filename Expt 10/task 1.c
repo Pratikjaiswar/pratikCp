85510e3776dfacae0c9de048a8bfcce48927089c
Pratikjaiswar <pratikjaiswar@eng.rizvi.edu.in>
2024-12-06 22:21:15 +0530

    Update task 1.c

--- Expt 10/task 1.c
+++ Expt 10/task 1.c
@@ -5,3 +5,24 @@ EXP NO : 10.1
  UIN :241S077
  Roll.No:59
  CLASS & DIV :F.E -ECS ENG(E3) */
+// adding two numbers using pointers
+#include <stdio.h>
+int main() {
+ int num1, num2, sum;
+ int *ptr1, *ptr2, *ptrSum;
+ printf("Enter the first number: ");
+ scanf("%d", &num1);
+ printf("Enter the second number: ");
+ scanf("%d", &num2);
+ ptr1 = &num1;
+ ptr2 = &num2;
+ ptrSum = &sum;
+ *ptrSum = *ptr1 + *ptr2;
+ printf("Sum of %d and %d is %d\n", *ptr1, *ptr2, *ptrSum);
+ return 0;
+}
+/*
+OUTPUT:- Enter the first number: 10
+Enter the second number: 33
+Sum of 10 and 33 is 43
+*/
